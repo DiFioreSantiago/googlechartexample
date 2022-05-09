9666e3892dcbecbc9098325990407c2cd4191ca1
Santiago Di Fiore <difiore.san@gmail.com>
2022-05-09 13:47:34 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleChart from './googlechart';
+
+function App() {
+
+  return (
+    <div className="App">
+      <GoogleChart />
+    </div>
+  )
+}
+
+export default App
